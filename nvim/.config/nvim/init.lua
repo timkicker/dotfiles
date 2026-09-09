2077dfa5b09d69ceeeb91e8001a53ed69b4889e1
Tim Jochen Kicker <tim@kicker.dev>
2026-09-09 22:36:41 +0300

    ydotool service + config cleanup

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -194,8 +194,8 @@ require("lazy").setup({
   { "nvim-lualine/lualine.nvim",
     config = function()
       local theme = {
-        normal   = { a = { fg="#000000", bg=C.green , gui="bold" }, c = { fg=C.fg, bg=C.bg2 } },
-        insert   = { a = { fg="#000000", bg=C.orange, gui="bold" } },
+        normal   = { a = { fg="#000000", bg=C.orange, gui="bold" }, c = { fg=C.fg, bg=C.bg2 } },
+        insert   = { a = { fg="#000000", bg=C.green , gui="bold" } },
         visual   = { a = { fg="#000000", bg=C.pink  , gui="bold" } },
         replace  = { a = { fg="#000000", bg=C.red   , gui="bold" } },
         command  = { a = { fg="#000000", bg=C.red   , gui="bold" } },
